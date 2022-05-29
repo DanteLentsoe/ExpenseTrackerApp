7991565c7daf751ac53c307aa9bc3c1199a786fd
dantelentsoe <dllentsoe@gmail.com>
2022-05-29 21:58:57 +0200

    refactor: update dimentsion api

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { VStack, Box, Divider, Input, Icon } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
 
 interface IQuerysetQuery {
   setQuery: Dispatch<SetStateAction<string>>;
@@ -9,24 +10,18 @@ interface IQuerysetQuery {
 
 const SearchBar = ({ searchQuery, setQuery }: IQuerysetQuery) => {
   return (
-    <VStack
-      my="4"
-      space={5}
-      w="100%"
-      maxW="300px"
-      divider={
-        <Box px="2">
-          <Divider />
-        </Box>
-      }>
+    <VStack my="4" space={5} w="100%" maxW="330px" style={styles.inputStyles}>
       <VStack w="100%" space={5} alignSelf="center">
         <Input
           placeholder="Search Expenses "
           width="100%"
-          borderRadius="4"
           py="3"
           px="1"
-          fontSize="14"
+          fontSize="15"
+          borderWidth="0"
+          borderRadius={40}
+          overflowX={"hidden"}
+          overflow="hidden"
           value={searchQuery}
           onChangeText={(text) => setQuery(text)}
           InputLeftElement={
@@ -46,3 +41,18 @@ const SearchBar = ({ searchQuery, setQuery }: IQuerysetQuery) => {
 };
 
 export default SearchBar;
+
+const styles = StyleSheet.create({
+  inputStyles: {
+    borderRadius: 40,
+    backgroundColor: "#ffffff",
+    shadowColor: "#222222",
+    elevation: 4,
+    shadowOffset: {
+      height: 4,
+      width: 0,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+  },
+});
