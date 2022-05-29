5f3eb689ef7352a0d3d4345b9e5041494419f525
dantelentsoe <dllentsoe@gmail.com>
2022-05-29 16:43:01 +0200

    feat: add render logic for expenses

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,21 +1,37 @@
 import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "../components/EditScreenInfo";
+import Theme from "../utils/theme";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import { ExpenseContext } from "../store/ExenpenseProvider";
+import { useContext } from "react";
 
 export default function HomeScreen({
   navigation,
 }: RootTabScreenProps<"HomeScreen">) {
+  const expenseDataTx = useContext(ExpenseContext);
+
+  const expenseInfo = expenseDataTx.expenses;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Track Expenses </Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/HomeScreen.tsx" />
+      <>
+        <Text style={styles.title}>Track Expenses </Text>
+        {expenseInfo.map((item, index) => {
+          return (
+            <View>
+              <View style={styles.expenseContainer} key={index}>
+                <View style={styles.internalContainer}>
+                  <Text>{item.title}</Text>
+                  <Text>{item.expenseCategory}</Text>
+                </View>
+                <View style={styles.amountContainer}>
+                  <Text>{item.amount}</Text>
+                </View>
+              </View>
+            </View>
+          );
+        })}
+      </>
     </View>
   );
 }
@@ -24,7 +40,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
   title: {
     fontSize: 20,
@@ -34,5 +49,28 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  expenseContainer: {
+    backgroundColor: Theme.colors.primary,
+    width: 333,
+    height: 70,
+    borderRadius: 40,
+    top: 12,
+    marginBottom: 10,
+    padding: 12,
+  },
+  internalContainer: {
+    flexDirection: "row",
+    backgroundColor: "transparent",
+  },
+  amountContainer: {
+    left: 250,
+    backgroundColor: "pink",
+    padding: 8,
+    borderTopRightRadius: 40,
+    borderBottomRighttRadius: 40,
+    height: 70,
+    width: 70,
+    top: -30,
   },
 });
