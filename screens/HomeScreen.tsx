ce2b4da96b4f8e8e7bd95f477b5c02fc45614322
dantelentsoe <dllentsoe@gmail.com>
2022-05-29 20:05:51 +0200

    refactor: update svg sizing

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,8 +3,45 @@ import Theme from "../utils/theme";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 import { IExpense } from "../constants/types";
+import { MaterialIcons } from "@expo/vector-icons";
 import { ExpenseContext } from "../store/ExenpenseProvider";
 import { useContext } from "react";
+import { VStack, Box, Divider, Input, Icon } from "native-base";
+import AddFilesSVG from "../assets/SVG/AddFilesSVG";
+const SearchBar = () => {
+  return (
+    <VStack
+      my="4"
+      space={5}
+      w="100%"
+      maxW="300px"
+      divider={
+        <Box px="2">
+          <Divider />
+        </Box>
+      }>
+      <VStack w="100%" space={5} alignSelf="center">
+        <Input
+          placeholder="Search Expenses "
+          width="100%"
+          borderRadius="4"
+          py="3"
+          px="1"
+          fontSize="14"
+          InputLeftElement={
+            <Icon
+              m="2"
+              ml="3"
+              size="6"
+              color="gray.400"
+              as={<MaterialIcons name="search" />}
+            />
+          }
+        />
+      </VStack>
+    </VStack>
+  );
+};
 
 export default function HomeScreen({
   navigation,
@@ -24,24 +61,40 @@ export default function HomeScreen({
   return (
     <View style={styles.container}>
       <>
+        <SearchBar />
         <Text style={styles.title}>
-          Track Expenses {expenseTotal.reduce(reducer)}
+          Track Expenses{" "}
+          {expenseTotal.length > 0 ? expenseTotal.reduce(reducer) : "😅"}
         </Text>
-        {expenseInfo.map((item, index) => {
-          return (
-            <View>
-              <View style={styles.expenseContainer} key={index}>
-                <View style={styles.internalContainer}>
-                  <Text>{item.title}</Text>
-                  <Text>{item.expenseCategory}</Text>
-                </View>
-                <View style={styles.amountContainer}>
-                  <Text style={styles.amountText}>{item.amount}</Text>
+        {expenseTotal.length > 0 ? (
+          expenseInfo.map((item: IExpense, index) => {
+            return (
+              <View>
+                <View style={styles.expenseContainer} key={index}>
+                  <View style={styles.internalContainer}>
+                    {/* if(snippet.length > 1024) {
+text = snippet.substring(0, 1024)//cuts to 1024
+last = text.lastIndexOf(" ")//gets last space (to avoid cutting the middle of a word)
+text = text.substring(0, last)//cuts from last space (to avoid cutting the middle of a word)
+text = text + ` (...)`//adds (...) at the end to show that it's cut
+} */}
+                    <Text style={styles.expenseTitle}>{item.title}</Text>
+                    <Text style={styles.expenseType}>
+                      {item.expenseCategory}
+                    </Text>
+                  </View>
+                  <View style={styles.amountContainer}>
+                    <Text style={styles.amountText}>{item.amount}</Text>
+                  </View>
                 </View>
               </View>
-            </View>
-          );
-        })}
+            );
+          })
+        ) : (
+          <View style={styles.svgIllustrator}>
+            <AddFilesSVG />
+          </View>
+        )}
       </>
     </View>
   );
@@ -62,6 +115,10 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  svgIllustrator: {
+    height: 450,
+    width: 280,
   },
   expenseContainer: {
     backgroundColor: Theme.colors.primary,
@@ -86,9 +143,17 @@ const styles = StyleSheet.create({
     width: 70,
     justifyContent: "center",
     alignItems: "center",
-    top: -26,
+    top: -29,
   },
-
+  expenseTitle: {
+    fontSize: 14,
+    top: 12,
+  },
+  expenseType: {
+    fontSize: 14,
+    top: 12,
+    left: 12,
+  },
   amountText: {
     color: "#ffffff",
   },
